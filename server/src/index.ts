f07ca91ca5fc22bfa603dfcc10a22fef29eee247
kam1kazy <t2nda@yandex.ru>
2024-08-10 05:00:08 +0300

    поставил doker+postress & prisma (надо настроить) 4:59

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 // Плагины для сервера
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { yoga } from '@elysiajs/graphql-yoga'
 import { cors } from '@elysiajs/cors'
 
 // Локальные подключения
 import { typeDefinitions } from './graphql/schema'
+import { PrismaClient } from '@prisma/client'
+
 import { resolvers } from './graphql/resolvers'
 import { lyrics } from './graphql/data'
+
+const db = new PrismaClient()
 
 const app = new Elysia()
   .use(cors())
@@ -22,7 +26,25 @@ const app = new Elysia()
   .get('/', () => {
     lyrics
   })
-  .post('/', ({ body }) => body)
+
+  .post(
+    '/sign-up',
+    async ({ body }) =>
+      db.user.create({
+        data: body,
+      }),
+    {
+      body: t.Object({
+        id: t.Number(),
+        text: t.String(),
+        is_reaction: t.Boolean(),
+        paragraph_count: t.Number(),
+        word_count: t.Number(),
+        hashtag_count: t.Number(),
+        hashtags: t.Array(t.String()),
+      }),
+    }
+  )
   .listen(4000)
 
 export { app }
