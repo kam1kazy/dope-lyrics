f8952dd6236af5a197dddfe211b793271312f80d
kam1kazy <t2nda@yandex.ru>
2024-08-15 22:47:35 +0300

    Удаление файлов .env

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,6 +8,27 @@ import { AddLyric } from '@/components/addLyric'
 import { LyricList } from '@/components/lyricList'
 
 export default function Home() {
+  fetch('http://localhost:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+          query Trap {
+            lyrics {
+              id
+            }
+          }
+        `,
+      variables: {
+        now: new Date().toISOString(),
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .then((result) => console.log(result))
+
   return (
     <ApolloProvider client={client}>
       <ChakraProvider>
