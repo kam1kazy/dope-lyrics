d3546419bdec117281ee1308a768642d7ca43982
kam1kazy <t2nda@yandex.ru>
2024-08-16 01:32:05 +0300

    убрал -y и добавил миграции

---
+++ server/prisma/migrations/20240815223102_git_add/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT,
+    "password" TEXT,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Lyric" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "is_reaction" BOOLEAN,
+    "paragraph_count" INTEGER,
+    "word_count" INTEGER,
+    "hashtag_count" INTEGER,
+    "hashtags" INTEGER[],
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Lyric_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Hashtag" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "lyricId" INTEGER,
+
+    CONSTRAINT "Hashtag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Hashtag_text_key" ON "Hashtag"("text");
