1981038513d1ac907f6bc400f16a9ed08aeae4bb
kam1kazy <t2nda@yandex.ru>
2024-09-23 02:39:30 +0300

    Уменьшил меню

--- client/src/components/menu/settingList/settingList.tsx
+++ client/src/components/menu/settingList/settingList.tsx
@@ -33,50 +33,44 @@ export default function SettingList({ isOpen }: { isOpen: boolean }) {
         fontSize='14px'
         boxShadow={'0px 10px 26px 20px rgba(34, 60, 80, 0.35)'}
       >
-        <Heading fontSize={'18px'} mb={'10px'}>
-          Отображение
-        </Heading>
-        <Divider />
+        {/*
+          <Heading fontSize={'18px'} mb={'10px'}>
+            Отображение
+          </Heading>
+          <Divider />
 
-        <Box
-          display={'flex'}
-          flexWrap={'wrap'}
-          justifyContent={'space-between'}
-        >
-          {/* Row */}
-          <Text alignContent={'center'}>Разделитель:</Text>
-          <RadioSelect settings={['Выкл', '***', '...', '----']} />
+          <Box
+            display={'flex'}
+            flexWrap={'wrap'}
+            justifyContent={'space-between'}
+          >
+            <Text alignContent={'center'}>Разделитель:</Text>
+            <RadioSelect settings={['Выкл', '***', '...', '----']} />
 
-          {/* Row */}
-          <Text alignContent={'center'}>Исправление грамматики (β):</Text>
-          <RadioSelect settings={['Вкл', 'Выкл']} />
-        </Box>
+            <Text alignContent={'center'}>Исправление грамматики (β):</Text>
+            <RadioSelect settings={['Вкл', 'Выкл']} />
+          </Box>
 
-        <Box as={SimpleGrid} columns={{ base: 4 }} columnGap={'10px'}>
-          {/* Row */}
-          <Text alignContent={'center'}>Хэштеги:</Text>
-          <RadioSelect settings={['Вкл', 'Выкл', 'Mini']} />
+          <Box as={SimpleGrid} columns={{ base: 4 }} columnGap={'10px'}>
+            <Text alignContent={'center'}>Хэштеги:</Text>
+            <RadioSelect settings={['Вкл', 'Выкл', 'Mini']} />
 
-          {/* Row */}
-          <Text alignContent={'center'}>Шрифт:</Text>
-          <RadioSelect settings={['Font', 'Font2', 'Font3']} />
+            <Text alignContent={'center'}>Шрифт:</Text>
+            <RadioSelect settings={['Font', 'Font2', 'Font3']} />
 
-          {/* Row */}
-          <Text alignContent={'center'}>Хэштеги:</Text>
-          <RadioSelect settings={['Вкл', 'Выкл', 'Mini']} />
+            <Text alignContent={'center'}>Хэштеги:</Text>
+            <RadioSelect settings={['Вкл', 'Выкл', 'Mini']} />
 
-          {/* Row */}
-          <Text alignContent={'center'}>Шрифт:</Text>
-          <RadioSelect settings={['Font', 'Font2', 'Font3']} />
+            <Text alignContent={'center'}>Шрифт:</Text>
+            <RadioSelect settings={['Font', 'Font2', 'Font3']} />
 
-          {/* Row */}
-          <Text alignContent={'center'}>Реакции:</Text>
-          <RadioSelect settings={['Вкл', 'Выкл', 'Mini']} />
+            <Text alignContent={'center'}>Реакции:</Text>
+            <RadioSelect settings={['Вкл', 'Выкл', 'Mini']} />
 
-          {/* Row */}
-          <Text alignContent={'center'}>Размер:</Text>
-          <RadioSelect settings={['14', '16', '18']} />
-        </Box>
+            <Text alignContent={'center'}>Размер:</Text>
+            <RadioSelect settings={['14', '16', '18']} />
+          </Box>
+        */}
 
         <Heading fontSize={'18px'} mb={'10px'} mt={'20px'}>
           Фильтры
@@ -111,7 +105,7 @@ export default function SettingList({ isOpen }: { isOpen: boolean }) {
           </NumberInput>
 
           {/* Row */}
-          <Text alignContent={'center'}>Наименование хэштега</Text>
+          <Text alignContent={'center'}>Наименование тега</Text>
           <Input width={'90px'} placeholder='Tags' />
 
           {/* Row */}
