3c681c5573f1a6704712b2a0a36e964368feaa49
d.mavlyutov <d.mavlyutov@v6services.ru>
2025-02-15 14:05:18 +0300

    BOT: Поправил использование и запись env

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,15 +1,31 @@
 import process from 'node:process'
+import dotenv from 'dotenv'
+import path from 'path'
 
-const API_ID = Number.parseInt(process.env.API_ID!)
-const API_HASH = process.env.API_HASH!
-const BOT_TOKEN = process.env.BOT_TOKEN!
-const BOT_PHONE = process.env.BOT_PHONE!
-const BOT_PASS = process.env.BOT_PASS!
-const BOT_CHAT_ID = Number.parseInt(process.env.BOT_CHAT_ID!)
-const BOT_CHANNEL_ID = Number.parseInt(process.env.BOT_CHANNEL_ID!)
+// Загружаем переменные окружения из .env файла
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
+const API_ID = Number.parseInt(process.env.API_ID ?? '')
+const API_HASH = process.env.API_HASH ?? ''
+const BOT_TOKEN = process.env.BOT_TOKEN ?? ''
+const BOT_PHONE = process.env.BOT_PHONE ?? ''
+const BOT_PASS = process.env.BOT_PASS ?? ''
+const BOT_CHAT_ID = Number.parseInt(process.env.BOT_CHAT_ID ?? '')
+const BOT_CHANNEL_ID = Number.parseInt(process.env.BOT_CHANNEL_ID ?? '')
+
+// Проверяем все необходимые переменные окружения
 if (Number.isNaN(API_ID) || !API_HASH) {
-  throw new Error('API_ID or API_HASH not set!')
+  throw new Error('API_ID или API_HASH не установлены!')
+}
+
+if (
+  !BOT_TOKEN ||
+  !BOT_PHONE ||
+  !BOT_PASS ||
+  Number.isNaN(BOT_CHAT_ID) ||
+  Number.isNaN(BOT_CHANNEL_ID)
+) {
+  throw new Error('Отсутствуют необходимые переменные окружения для бота!')
 }
 
 export {
