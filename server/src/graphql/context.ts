217bc57240ab82ab42f46a44240e79e1111e57c9
d.mavlyutov <d.mavlyutov@v6services.ru>
2025-02-22 23:06:24 +0300

    SERVER: Убрал логи

--- server/src/graphql/context.ts
+++ server/src/graphql/context.ts
@@ -3,13 +3,11 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
 })
-console.log('context.ts Prisma initialized with:', process.env.DATABASE_URL)
 
 export type GraphQLContext = {
   prisma: PrismaClient
 }
 
 export async function createContext(): Promise<GraphQLContext> {
-  console.log('Creating context')
   return { prisma }
 }
