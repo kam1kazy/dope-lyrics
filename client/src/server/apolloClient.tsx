73670ef808ea757ba566c13fce21e60ded37c1eb
d.mavlyutov <d.mavlyutov@v6services.ru>
2025-02-23 01:18:46 +0300

    CLIENT: поправил env

--- client/src/server/apolloClient.tsx
+++ client/src/server/apolloClient.tsx
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+console.log(process.env.NEXT_PUBLIC_BASE_URL)
 const client = new ApolloClient({
-  uri: `${process.env.BASE_URL}/graphql`,
+  uri:
+    `${process.env.NEXT_PUBLIC_BASE_URL}/graphql` ||
+    'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 })
 
