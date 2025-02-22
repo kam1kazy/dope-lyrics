6147f2e359406cb74feea05f4c100fdd915251f6
d.mavlyutov <d.mavlyutov@v6services.ru>
2025-02-22 23:11:21 +0300

    SERVER: Расширил команды для бота |проверка на isAdmin

--- server/src/mtcute/commands/index.ts
+++ server/src/mtcute/commands/index.ts
@@ -1,5 +1,5 @@
 // КОНСТАНТЫ
-import { BotKeyboard } from '@mtcute/core'
+import { BotKeyboard, tl } from '@mtcute/core'
 import * as env from '../../env'
 
 // HANDLERS
@@ -9,61 +9,165 @@ import { sendToBotChat } from '../../handlers/handlers'
 // TYPES
 import { TypeBotClient } from '../index'
 
+// HOOKS
+import { useAdminCheck } from '~/hooks/useAdminCheck'
+
+// PRISMA
+import seed from '../../../prisma/script/seed'
+
+// Дата базы
+import { PrismaClient } from '@prisma/client'
+import { prismaService } from '~/services/db'
+
+// TYPES
+import {
+  CallbackQueryContext,
+  filters,
+  MessageContext,
+} from '@mtcute/dispatcher'
+
+const db = new PrismaClient()
+
 const chatId = env.BOT_CHAT_ID
 const channelId = env.BOT_CHANNEL_ID
 
 interface ICommandChat {
-  tg: TypeBotClient
-  msg: any
-}
-
-// Команды для бота
-const options = {
-  reply_markup: {
-    inline_keyboard: [
-      [
-        {
-          text: 'Открыть приложение',
-          url: 'https://violet-cougars-listen.loca.lt',
-        },
-      ],
-    ],
-  },
-}
-
-// Получаем историю чата
-const commandChatHistory = async ({ tg, msg }: ICommandChat) => {
-  await msg.delete()
-  await getChatHistory({ tg, chatId: channelId })
-    .then(() => {
-      sendToBotChat({ tg, chatId, text: 'MTCUTE: 📥 История чата получена' })
-    })
-    .catch(() => {
-      sendToBotChat({
-        tg,
-        chatId,
-        text: 'MTCUTE: 🛑 Ошибка при получении истории',
-      })
-    })
+  tg?: TypeBotClient | null
+  tgAdmin?: TypeBotClient | null
+  msg:
+    | filters.Modify<MessageContext, { command: string[] }>
+    | CallbackQueryContext
+  keyboard?: tl.TypeKeyboardButton[][]
 }
 
 // Получаем ID чата
 const commandChatId = async ({ tg, msg }: ICommandChat) => {
-  await msg.delete()
-  const text = 'MTCUTE: 💳 Chat ID: ' + msg.chat.id
-  sendToBotChat({ tg, chatId, text })
-}
+  if (!tg) return
+  await tg
+    .deleteMessagesById(msg.chat.id, [msg.id as number])
+    .catch(console.error)
 
-// Открываем приложение
-const commandStart = async ({ tg, msg }: ICommandChat) => {
-  await msg.delete()
-  const text = 'MTCUTE: 📱 Вы хотите открыть приложение?'
-
-  await tg.sendText(msg.chat.id, text, {
-    replyMarkup: BotKeyboard.inline([
-      [BotKeyboard.url('Запустить', 'https://violet-cougars-listen.loca.lt')],
-    ]),
+  await useAdminCheck({
+    tg,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    action: async () => {
+      const text = '💳 Chat ID: ' + msg.chat.id
+      sendToBotChat({ tg, chatId: msg.chat.id, text })
+    },
   })
 }
 
-export { commandChatHistory, commandChatId, commandStart }
+// Открываем приложение
+const commandStartApp = async ({ tg, msg }: ICommandChat) => {
+  if (!tg) return
+  await useAdminCheck({
+    tg,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    action: async () => {
+      const text = '📱 Вы хотите открыть приложение?'
+      await tg.sendText(msg.chat.id, text, {
+        replyMarkup: BotKeyboard.inline([
+          [
+            BotKeyboard.url(
+              'Запустить',
+              'https://orange-trains-shake.loca.lt/'
+            ),
+          ],
+        ]),
+      })
+    },
+  })
+}
+
+// Управление базой данных
+const commandStartBd = async ({ tg, msg, keyboard }: ICommandChat) => {
+  if (!tg) return
+  await useAdminCheck({
+    tg,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    action: async () => {
+      const text = '⚡️ Управление базой данных'
+      await tg.sendText(msg.chat.id, text, {
+        replyMarkup: BotKeyboard.inline(keyboard ?? []),
+      })
+    },
+  })
+}
+
+// Получаем историю чата
+const commandChatHistory = async ({ tgAdmin, msg }: ICommandChat) => {
+  if (!tgAdmin) return
+  await useAdminCheck({
+    tg: tgAdmin,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    action: async () => {
+      await getChatHistory({ tg: tgAdmin, chatId: channelId })
+    },
+  })
+}
+
+// Заполняем базу данных
+const seedToBD = async ({ tgAdmin, msg, tg }: ICommandChat) => {
+  if (!tgAdmin) return
+
+  await useAdminCheck({
+    tg: tgAdmin,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    msgCallback: msg as CallbackQueryContext,
+    action: async () => {
+      sendToBotChat({
+        tg: tgAdmin,
+        chatId,
+        text: '🌱 Начался посев...',
+      })
+
+      await seed()
+    },
+  })
+}
+
+// Очищаем базу данных
+const clearBD = async ({ tgAdmin, msg }: ICommandChat) => {
+  if (!tgAdmin) return
+  await useAdminCheck({
+    tg: tgAdmin,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    msgCallback: msg as CallbackQueryContext,
+    action: async () => {
+      sendToBotChat({
+        tg: tgAdmin,
+        chatId,
+        text: '🧹 Началась очистка базы...',
+      })
+      await prismaService.clearDatabase()
+    },
+  })
+}
+
+// Получение статистики
+const getStats = async ({ tgAdmin, msg }: ICommandChat) => {
+  if (!tgAdmin) return
+  await useAdminCheck({
+    tg: tgAdmin,
+    msg: msg as filters.Modify<MessageContext, { command: string[] }>,
+    action: async () => {
+      const stats = await prismaService.getStats()
+
+      sendToBotChat({
+        tg: tgAdmin,
+        chatId,
+        text: `📊 Статистика: \n\nUsers: ${stats?.users} \nLyrics: ${stats?.lyrics}`,
+      })
+    },
+  })
+}
+
+export {
+  commandChatHistory,
+  commandChatId,
+  commandStartApp,
+  commandStartBd,
+  seedToBD,
+  clearBD,
+  getStats,
+}
--- server/src/mtcute/index.ts
+++ server/src/mtcute/index.ts
@@ -1,38 +1,69 @@
 // TELEGRAM BOT
-import { TelegramClient } from '@mtcute/bun'
-import { Dispatcher, filters } from '@mtcute/dispatcher'
+import { TelegramClient, BotKeyboard, tl } from '@mtcute/bun'
+import { CallbackDataBuilder, Dispatcher, filters } from '@mtcute/dispatcher'
 import path from 'path'
 
 // HANDLERS
-import { commandChatHistory, commandChatId, commandStart } from './commands'
+import {
+  clearBD,
+  commandChatId,
+  commandStartApp,
+  commandStartBd,
+  seedToBD,
+} from './commands'
 
 // КОНСТАНТЫ
 import * as env from '../env'
+import { prismaService } from '~/services/db'
+import { getChatHistory } from '~/handlers/getChatHistory'
+import { sendToBotChat } from '~/handlers/handlers'
 
 const phone = env.BOT_PHONE
 const pass = env.BOT_PASS
+const botToken = env.BOT_TOKEN
+const botType = env.BOT_TYPE
+const channelId = env.BOT_CHANNEL_ID
 
-// Создаем клиент
-export const tg = new TelegramClient({
+// Создаем и инициализируем основного бота
+export let tg = new TelegramClient({
   apiId: env.API_ID,
   apiHash: env.API_HASH,
   storage: path.resolve(__dirname, '../../bot-data/session'),
 })
 
-export type TypeBotClient = typeof tg
-
-// Диспетчер событий
-const dp = Dispatcher.for(tg)
-console.log('MTCUTE: Database connection string:', process.env.DATABASE_URL)
-
-export type TypeBotDispatcher = typeof dp
-
-// Авторизуемся в боте
 const self = await tg
   .start({
+    botToken: botToken,
+  })
+  .then((user) => {
+    console.log('MTCUTE: 🤖 Бот запущен')
+    return user
+  })
+  .catch((error) => {
+    console.error('MTCUTE: ❌ Ошибка при запуске бота:', error)
+  })
+
+// Диспетчер событий
+export type TypeBotClient = typeof tg
+
+const dp = Dispatcher.for(tg)
+export type TypeBotDispatcher = typeof dp
+
+// Создаем и инициализируем админ-клиент
+export let tgAdmin: TelegramClient | null = null
+
+if (botType === 'admin') {
+  tgAdmin = new TelegramClient({
+    apiId: env.API_ID,
+    apiHash: env.API_HASH,
+    storage: path.resolve(__dirname, '../../bot-data/sessionAdmin/session'),
+  })
+
+  // Добавляем инициализацию tgAdmin
+  await tgAdmin.start({
     phone: phone,
     code: async () => {
-      const code = await prompt('MTCUTE: 🙈 Введите код:')
+      const code = await prompt('MTCUTE: 🙈 Введите код для админа:')
       if (code === null) {
         throw new Error('MTCUTE: ❌ Отменено пользователем\n\n')
       }
@@ -40,22 +71,123 @@ const self = await tg
     },
     password: pass,
   })
-  .then((self) => {
-    console.log('\nMTCUTE: 🤖 Вошел в систему как -', self.username)
-  })
-  .catch((error) => {
-    console.error('\nMTCUTE: 🛑 Не вошел в систему\n\n', error)
-  })
+  console.log('MTCUTE: 🤖 Админ вошел в систему')
+}
 
-// Получаем историю чата
-dp.onNewMessage(filters.command('chathistory'), async (msg) =>
-  commandChatHistory({ tg, msg })
-)
 // Получаем ID чата
 dp.onNewMessage(filters.command('chatid'), async (msg) =>
   commandChatId({ tg, msg })
 )
 // Открываем приложение
 dp.onNewMessage(filters.command('app'), async (msg) =>
-  commandStart({ tg, msg })
+  commandStartApp({ tg, msg })
 )
+
+const BdButton = new CallbackDataBuilder('bd', 'id', 'action')
+
+const markup: tl.TypeKeyboardButton[][] = [
+  [
+    BotKeyboard.callback(
+      '🏄‍♂️ Статистика',
+      BdButton.build({ id: '1', action: 'stats' })
+    ),
+    BotKeyboard.callback(
+      '🏄‍♂️ Парсинг чата',
+      BdButton.build({ id: '2', action: 'history' })
+    ),
+  ],
+  [
+    BotKeyboard.callback(
+      '🏄‍♂️ Посев',
+      BdButton.build({ id: '3', action: 'seed' })
+    ),
+    BotKeyboard.callback(
+      '🏄‍♂️ Очистка',
+      BdButton.build({ id: '4', action: 'clear' })
+    ),
+  ],
+]
+
+// Управление базой данных
+dp.onNewMessage(filters.command('bd'), async (msg) =>
+  commandStartBd({ tg, msg, keyboard: markup })
+)
+
+dp.onCallbackQuery(async (query) => {
+  if (!query.data) return
+  const data = BdButton.parse(Buffer.from(query.data).toString())
+  if (!data) return
+
+  const action = data.action
+  const chatId = query.chat.id
+
+  if (!chatId) {
+    await query.answer({ text: '❌ Ошибка: чат не найден', alert: true })
+    return
+  }
+  if (!tgAdmin) {
+    await query.answer({
+      text: '❌ Ошибка: админ не авторизован',
+      alert: true,
+    })
+    return
+  }
+  switch (action) {
+    case 'stats':
+      query.answer({ text: '⏳ Получение статистики...' })
+      const stats = await prismaService.getStats()
+      if (stats) {
+        sendToBotChat({
+          tg,
+          chatId: chatId,
+          text: `📊 Статистика:\n\nUsers: ${stats?.users}\nLyrics: ${stats?.lyrics}`,
+        })
+      }
+      break
+
+    case 'history':
+      query.answer({ text: '⏳ Получение истории...' })
+      sendToBotChat({
+        tg,
+        chatId: chatId,
+        text: '🔍 Получение истории чата...',
+      })
+      const success = await getChatHistory({ tg: tgAdmin, chatId: channelId })
+      if (success) {
+        sendToBotChat({
+          tg,
+          chatId: chatId,
+          text: '📥 История чата получена',
+        })
+      }
+      break
+
+    case 'seed':
+      query.answer({ text: '⏳ Начался посев...' })
+      await seedToBD({ tgAdmin: tgAdmin, msg: query }).then(() => {
+        sendToBotChat({
+          tg,
+          chatId: chatId,
+          text: '✅ Посев завершен',
+        })
+      })
+
+      break
+
+    case 'clear':
+      query.answer({ text: '⏳ Началась очистка...' })
+      await clearBD({ tgAdmin: tgAdmin, msg: query })
+      sendToBotChat({
+        tg,
+        chatId: chatId,
+        text: '🧹 Очистка завершена',
+      })
+      break
+
+    default:
+      await query.answer({
+        text: '❌ Неизвестная команда',
+        alert: true,
+      })
+  }
+})
