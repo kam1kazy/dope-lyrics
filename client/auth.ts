f740156d0743b3b9c5b63b33cde5c6818e1197f0
kam1kazy <t2nda@yandex.ru>
2025-02-26 18:08:48 +0300

    CLIENT: Вынес клиент призмы в lib

--- client/auth.ts
+++ client/auth.ts
@@ -1,10 +1,8 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/lib/prisma'
 
 import TelegramProvider from '@/providers/telegramProvider'
-
-export const prisma = new PrismaClient()
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
